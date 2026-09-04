de86b34b741e1589a30f68f78090c698fea468ae
Bianca Alves <balvesbia9@gmail.com>
2026-09-03 22:01:30 -0300

    calculadora.java

---
+++ galoo 05/Calculadora.java
@@ -0,0 +1,30 @@
+public class Calculadora {
+    public int somar (int numero1, int numero2){
+       int resultado = numero1 + numero2;
+       return resultado;
+    
+      
+    }
+    
+
+    public int subtrair (int numero1, int numero2){
+       int resultado = numero1 - numero2;
+       return resultado;
+    
+      
+    
+}
+
+public int multiplicar  (int numero1, int numero2){
+       int resultado = numero1 * numero2;
+       return resultado;
+    
+      
+    }
+public int dividir  (int numero1, int numero2){
+       int resultado = numero1 / numero2;
+       return resultado;
+    
+      
+    }
+}
